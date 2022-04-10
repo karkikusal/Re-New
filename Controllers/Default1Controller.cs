a5fac0461e742770d4b6f80b61e7502ced5653f8
kusalkarki <karkikusal16@gmail.com>
2022-04-10 14:49:23 +0545

    Re-new

---
+++ Controllers/Default1Controller.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        // GET: Default1
+        employeeEntities db;
+        public Default1Controller()
+        {
+            db = new employeeEntities();
+        }
+        // GET: Default
+        public ActionResult Index1()
+        {
+            List<employee_salary_detail> data = db.employee_salary_detail.ToList();
+            return View(data);
+
+
+        }
+
+        public ActionResult Create()
+        {
+            var empList = db.emps.ToList();
+            //for option
+            //ViewBag.empsList = empsList;
+            ViewBag.employeeList = new SelectList(empList, "ID", "name");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(employee_salary_detail employee_Salary_Detail)
+        {
+            db.employee_salary_detail.Add(employee_Salary_Detail);
+            db.SaveChanges();
+            return RedirectToAction("Index1");
+        }
+        public ActionResult Edit(int id)
+        {
+            List<emp> Emplist = db.emps.ToList();
+            ViewBag.employeeList = new SelectList(Emplist, "ID", "name");
+            employee_salary_detail employee1 = db.employee_salary_detail.Find(id);
+            return View(employee1);
+        }
+        public ActionResult updateData(employee_salary_detail employee_Salary_Detail)
+        {
+            db.Entry(employee_Salary_Detail).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("index1");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            employee_salary_detail data = db.employee_salary_detail.Find(id);
+            db.employee_salary_detail.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("index1");
+        }
+    }
+}
---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        employeeEntities db;
+        public DefaultController()
+        {
+            db = new employeeEntities();
+        }
+        // GET: Default
+        public ActionResult Index()
+        {
+            List<emp> data = db.emps.ToList();
+            return View(data);
+
+
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult SaveData(emp emp)
+        {
+            db.emps.Add(emp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UpdateData(emp emp)
+        {
+            db.Entry(emp).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            emp data = db.emps.Find(id);//find data using primary key
+            //emp data=db.emp.First0rDefault(x=> x.id== id);
+            return View(data);
+        }
+        public ActionResult Delete(int id)
+        {
+            emp data = db.emps.Find(id);
+            db.emps.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
